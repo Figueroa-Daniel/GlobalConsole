0c4639f6540ae5ae8186a50717c78d2e7dceddd4
Figueroa <danifv02@gmail.com>
2026-08-02 21:03:58 +0200

    [GC] docs: añadir kdoc a la clase de pruebas SharedLogicDesktopTest

---
+++ shared/src/jvmTest/kotlin/org/example/globalconsole/SharedLogicDesktopTest.kt
@@ -0,0 +1,24 @@
+package org.example.globalconsole
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Pruebas unitarias específicas para la plataforma JVM/Escritorio ejecutadas a través de [kotlin.test].
+ *
+ * @author Daniel Figueroa Vidal
+ * @since 2026-08-02
+ */
+class SharedLogicDesktopTest {
+
+    /**
+     * Prueba de ejemplo para validar que el motor de pruebas en JVM funciona correctamente.
+     *
+     * @author Daniel Figueroa Vidal
+     * @since 2026-08-02
+     */
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
