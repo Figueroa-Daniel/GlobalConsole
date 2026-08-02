0861e8cb1c85b8fdc98f0d1aa1004365f12d9914
Figueroa <danifv02@gmail.com>
2026-08-02 21:02:24 +0200

    [GC] docs: añadir kdoc a la clase base Game

--- desktopApp/src/main/kotlin/org/example/globalconsole/generalDomain/entititys/Game.kt
+++ desktopApp/src/main/kotlin/org/example/globalconsole/generalDomain/entititys/Game.kt
@@ -1,4 +1,22 @@
 package org.example.globalconsole.generalDomain.entititys
 
-class Game {
-}
+/**
+ * Entidad base que representa un juego en el frontend de GlobalConsole.
+ * Define las propiedades comunes necesarias para mostrar e iniciar cualquier videojuego.
+ *
+ * @property id Identificador único del juego.
+ * @property name Nombre visible del juego.
+ * @property urlGameExecute Ruta o comando necesario para ejecutar el juego.
+ * @property image Ruta local o URL de la carátula del juego.
+ * @property platform Plataforma o emulador al que pertenece el juego.
+ *
+ * @author Daniel Figueroa Vidal
+ * @since 2026-08-02
+ */
+open class Game(
+    open val id: String,
+    open val name: String,
+    open val urlGameExecute: String,
+    open val image: String?,
+    open val platform: Platforms
+)
