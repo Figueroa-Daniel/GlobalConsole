b0db9e0c308b30afc3963865a5b559910c515204
Figueroa <danifv02@gmail.com>
2026-08-12 23:42:05 +0200

    [GC] feat: actualizar PresentationModule con dependencias de Heroic

--- desktopApp/src/main/kotlin/org/example/globalconsole/di/PresentationModule.kt
+++ desktopApp/src/main/kotlin/org/example/globalconsole/di/PresentationModule.kt
@@ -4,18 +4,29 @@ import org.example.globalconsole.presesentation.viewModel.home.HomeViewModel
 import org.example.globalconsole.presesentation.viewModel.settings.SettingsViewModel
 import org.koin.dsl.module
 
+/**
+ * Módulo de inyección de dependencias de la capa de presentación.
+ * Provee los ViewModels de la aplicación con todas sus dependencias inyectadas por Koin.
+ *
+ * @author Daniel Figueroa Vidal
+ * @since 2026-08-10
+ */
 val presentationModule = module {
     factory {
         HomeViewModel(
             getGamesP2UseCase = get(),
             executeGameP2UseCase = get(),
-            deleteGameP2UseCase = get()
+            deleteGameP2UseCase = get(),
+            executeHGLauncherUseCase = get(),
+            isHeroicEnabledUseCase = get()
         )
     }
     factory {
         SettingsViewModel(
             saveEmulatorPathUseCase = get(),
-            getEmulatorPathUseCase = get()
+            getEmulatorPathUseCase = get(),
+            isHeroicEnabledUseCase = get(),
+            saveHeroicEnabledUseCase = get()
         )
     }
 }
