0414bf2345ebb6a3a174dcb60be9ba64f665c3cb
Figueroa <danifv02@gmail.com>
2026-08-16 18:02:28 +0200

    [GC] feat: Añadir variables globales de Dolphin para juegos y ejecutable

--- desktopApp/src/main/kotlin/org/example/globalconsole/settings/SettingsPlatforms.kt
+++ desktopApp/src/main/kotlin/org/example/globalconsole/settings/SettingsPlatforms.kt
@@ -36,3 +36,20 @@ var ROUTE_MELONDS_GAMES: String? = null
  * @since 2026-08-15
  */
 var ROUTE_MELONDS_EXECUTABLE: String? = null
+
+/**
+ * Ruta del directorio local donde se encuentran almacenados los juegos de Wii/GameCube (Dolphin).
+ * Configurable en tiempo de ejecución.
+ *
+ * @author Daniel Figueroa Vidal
+ * @since 2026-08-16
+ */
+var ROUTE_DOLPHIN_GAMES: String? = null
+
+/**
+ * Ruta de instalación o binario ejecutable de Dolphin (si aplica para Windows).
+ *
+ * @author Daniel Figueroa Vidal
+ * @since 2026-08-16
+ */
+var ROUTE_DOLPHIN_EXECUTABLE: String? = null
