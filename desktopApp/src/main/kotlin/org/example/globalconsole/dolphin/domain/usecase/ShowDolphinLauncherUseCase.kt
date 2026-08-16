9f3492cb2e305270b63143d71b19fdcd48699189
Figueroa <danifv02@gmail.com>
2026-08-16 18:14:25 +0200

    [GC] Fix: retornar entidad DolphinLauncher en lugar de Dto

--- desktopApp/src/main/kotlin/org/example/globalconsole/dolphin/domain/usecase/ShowDolphinLauncherUseCase.kt
+++ desktopApp/src/main/kotlin/org/example/globalconsole/dolphin/domain/usecase/ShowDolphinLauncherUseCase.kt
@@ -2,6 +2,8 @@ package org.example.globalconsole.dolphin.domain.usecase
 
 import org.example.globalconsole.dolphin.data.dto.DolphinLauncherDto
 import org.example.globalconsole.dolphin.data.repository.DolphinRepository
+import org.example.globalconsole.dolphin.domain.entitys.DolphinLauncher
+import org.example.globalconsole.dolphin.data.mappers.toDomain
 
 /**
  * UseCase para obtener la información del launcher de Dolphin para ser mostrada.
@@ -10,7 +12,7 @@ import org.example.globalconsole.dolphin.data.repository.DolphinRepository
  * @since 2026-08-16
  */
 class ShowDolphinLauncherUseCase(private val repository: DolphinRepository) {
-    suspend operator fun invoke(): DolphinLauncherDto {
-        return repository.showDolphinLauncher()
+    suspend operator fun invoke(): DolphinLauncher {
+        return repository.showDolphinLauncher().toDomain()
     }
 }
